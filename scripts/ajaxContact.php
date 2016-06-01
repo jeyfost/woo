b11a1083e38e75313d281e859858ffb561546dd1
jeyfost <foster_andrew@tut.by>
2016-06-01 18:19:34 +0300

    Добавлена reCaptha в раздел обратной связи. UPD 2

--- scripts/ajaxContact.php
+++ scripts/ajaxContact.php
@@ -1,46 +1,42 @@
 <?php
 
 include("connect.php");
-include("getCurlData.php");
+require_once("recaptchaLib.php");
+
+$secret = "6LfNbyETAAAAACo25-rBaST_5KGAcceaZ-M9xfMN";
+$response = null;
+$reCaptcha = new ReCaptcha($secret);
 
 $name = trim(htmlspecialchars($_POST['name']));
 $email = trim(htmlspecialchars($_POST['email']));
 $theme = trim(htmlspecialchars($_POST['theme']));
 $text = trim(htmlspecialchars($_POST['message']));
 
+if($_POST['g-recaptcha-response']) {
+    $response = $reCaptcha->verifyResponse($_SERVER['REMOTE_ADDR'], $_POST['g-recaptcha-response']);
+}
+
 if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $recaptcha = $_POST['g-recaptcha-response'];
-    if(!empty($recaptcha)) {
-        $google_url = "https://www.google.com/recaptcha/api/siteverify";
-        $secret = "6LfNbyETAAAAACo25-rBaST_5KGAcceaZ-M9xfMN";
-        $ip = $_SERVER['REMOTE_ADDR'];
-        $url = $google_url."?secret=".$secret."&response=".$recaptcha."&remoteip=".$ip;
-        $res = getCurlData($url);
-        $res = json_decode($res, true);
+    if($response != null and $response->success) {
+        $from = $name;
+        $subject = "Сообщение с сайта WooWoo | ".$theme;
+        $to = "1@1.cc";
 
-        if($res['success']) {
-            $from = $name;
-            $subject = "Сообщение с сайта WooWoo | ".$theme;
-            $to = "1@1.cc";
+        $hash = md5(date('r', time()));
 
-            $hash = md5(date('r', time()));
+        $headers = "From: ".$from."\nReply-To: ".$email."\nMIME-Version: 1.0";
+        $headers .= "\nContent-Type: multipart/mixed; boundary = \"PHP-mixed-".$hash."\"\n\n";
 
-            $headers = "From: ".$from."\nReply-To: ".$email."\nMIME-Version: 1.0";
-            $headers .= "\nContent-Type: multipart/mixed; boundary = \"PHP-mixed-".$hash."\"\n\n";
+        $message = "--PHP-mixed-".$hash."\n";
+        $message .= "Content-Type: text/html; charset=\"windows-1251\"\n";
+        $message .= "Content-Transfer-Encoding: 8bit\n\n";
+        $message .= $text."\n";
+        $message .= "--PHP-mixed-".$hash."\n";
 
-            $message = "--PHP-mixed-".$hash."\n";
-            $message .= "Content-Type: text/html; charset=\"windows-1251\"\n";
-            $message .= "Content-Transfer-Encoding: 8bit\n\n";
-            $message .= $text."\n";
-            $message .= "--PHP-mixed-".$hash."\n";
-
-            if(@mail($to, $subject, $message, $headers)) {
-                echo "ok";
-            } else {
-                echo "failed";
-            }
+        if(@mail($to, $subject, $message, $headers)) {
+            echo "ok";
         } else {
-            echo "captcha";
+            echo "failed";
         }
     } else {
         echo "captcha";
